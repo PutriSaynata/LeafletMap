1abc074a468d146dc3d974d0155bbc8f294b0dd6
PutriSaynata <saynataputri@gmail.com>
2024-09-23 11:00:18 +0700

    Simbol dan Marker

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -50,10 +50,19 @@ export class HomePage {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-
-    const gramediaMarker = L.marker([-7.78333, 110.37450]).addTo(this.map);
-
-    // Menambahkan popup dengan gambar dan teks
+//marker
+const gramediaMarker = L.marker([-7.78333, 110.37450], {
+  icon: L.icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    shadowSize: [41, 41]
+  })
+}).addTo(this.map);
+  
+    //pop up
     const popupContent = `
       <b>Gramedia Sudirman</b><br>
       Yogyakarta, Indonesia.<br>
